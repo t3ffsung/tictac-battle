3034288d1e97f670ee5394adb6e701f6ae6ba55b
t3ffsung <mrsecret11118@gmail.com>
2026-03-03 14:55:27 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,22 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDWlbjE0Q-BP0_dWSkkJN1sPjpW0qfRVGc",
   authDomain: "tictac-battle.firebaseapp.com",
-  databaseURL: "https://tictac-battle-default-rtdb.firebaseio.com",
   projectId: "tictac-battle",
   storageBucket: "tictac-battle.firebasestorage.app",
   messagingSenderId: "238523804333",
-  appId: "1:238523804333:web:b63ad913e6ece503d17424"
+  appId: "1:238523804333:web:b63ad913e6ece503d17424",
+  databaseURL: "https://tictac-battle-default-rtdb.firebaseio.com"
 };
 
+// Initialize
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
 
-const auth = getAuth(app);
-signInAnonymously(auth)
-  .then(() => console.log("🔥 Firebase connected"))
-  .catch((error) => console.error(error));
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const database = getDatabase(app);
